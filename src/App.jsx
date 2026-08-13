4f3b06351f771affac4d52538b62bc1183a9b142
lenz-godie-11 <emmanueldaud358@gmail.com>
2026-08-14 00:46:23 +0300

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/media/media.jsx';
 import FmRadio from './components/fm-radio/fm-radio.jsx';
+import Footer from './components/extras/extras.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <FmRadio />
+      <Footer />
     </div>
   );
 }
