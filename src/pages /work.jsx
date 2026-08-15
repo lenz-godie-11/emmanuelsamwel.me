f80b4155bf6874496682fd0562f129b6d05d9546
lenz-godie-11 <emmanueldaud358@gmail.com>
2026-08-15 22:04:40 +0300

    new feature

---
+++ src/pages /work.jsx
@@ -0,0 +1,12 @@
+function Work () {
+
+    return (
+        <>
+        <section>
+            <div>
+                <h3>Work &Projects</h3>
+            </div>
+        </section>
+        </>
+    );
+}
